7fd7cab5ff6f10686336cffa2cbc94e58256887d
alexandregadaix-divido <86972204+alexandregadaix-divido@users.noreply.github.com>
2023-03-14 16:49:23 +0000

    Initial entry

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexandregadaix-divido/toolkit
+
+go 1.19
---
+++ tools_test.go
@@ -0,0 +1,13 @@
+package toolkit
+
+import "testing"
+
+func TestTools_randomString(t *testing.T) {
+	var testTools Tools
+
+	s := testTools.RandomString(10)
+
+	if len(s) != 10 {
+		t.Error("wrong length random string returned")
+	}
+}
